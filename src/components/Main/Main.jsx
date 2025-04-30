6f599cc08d0876c3a40e4045f59f4c8467817fd1
Luan Saboia <luan.saboia@gmail.com>
2025-04-30 17:08:40 -0300

    feature completed component
    finalizado a tela de front com alguns ajustes pendentes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -32,6 +32,20 @@ const Main = () => {
                     <img src={assets.code_icon} alt="Code icon" />
                 </div>
             </div>
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder="Enter a prompt here" />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+            </div>
+            <p className="bottom-info">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
         </div>
     </div>
   );
